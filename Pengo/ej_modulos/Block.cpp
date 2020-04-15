00af2cb790a2107b5c6d6da87df3501e69f5f340
Alex <33158086+AlejandroDCastro@users.noreply.github.com>
2020-04-15 13:15:05 +0200

    Update Block.cpp

--- Pengo/ej_modulos/Block.cpp
+++ Pengo/ej_modulos/Block.cpp
@@ -5,8 +5,6 @@
 Block::Block(sf::Texture* texture, unsigned int x, unsigned int y) {
 
     // Initial values
-    isBreaking = false;
-    isBroke    = false;
     direction  = -1;
     speed      = 30.0f;
 
@@ -26,57 +24,12 @@ Block::~Block() {
 
 
 
-void Block::Update(float deltaTime) {
-    if (!isBroke  &&  !isBreaking) {
-        switch (direction) {
-            case 0:
-                block->move(0, -speed*deltaTime);
-                break;
-            case 1:
-                block->move(speed*deltaTime, 0);
-                break;
-            case 2:
-                block->move(0, speed*deltaTime);
-                break;
-            case 3:
-                block->move(-speed*deltaTime, 0);
-                break;
-        }
-    } else if (isBreaking) {
-        if (clock.getElapsedTime().asSeconds() > 0.1f) {
-            sf::IntRect _cutout = block->getTextureRect();
-            _cutout.left += 32;
-            if (_cutout.left <= 288) {
-                block->setTextureRect(_cutout);
-            } else {
-                isBreaking = false;
-                isBroke    = true;
-            }
-            clock.restart();
-        }
-    }
-}
-
-
-
 void Block::Draw(sf::RenderWindow &window) {
     window.draw(*block);
 }
 
 
 
-void Block::breakDown() {
-    isBreaking = true;
-}
-
-
-
-bool Block::getBroke() {
-    return isBroke;
-}
-
-
-
-void Block::setDirection(int direction) {
+void Block::getPushed(int direction) {
     this->direction = direction;
 }
