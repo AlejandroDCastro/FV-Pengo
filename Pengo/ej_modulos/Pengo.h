59c04afa8782dd3298ba732dbd799645d00efe28
Alex <33158086+AlejandroDCastro@users.noreply.github.com>
2020-04-15 14:15:20 +0200

    Update Pengo.h

--- Pengo/ej_modulos/Pengo.h
+++ Pengo/ej_modulos/Pengo.h
@@ -1,1 +1,14 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include "Character.h"
 
+
+class Pengo : public Character {
+    
+    private:
+        
+  
+    public:
+        void Update(float deltaTime);
+  
+};
