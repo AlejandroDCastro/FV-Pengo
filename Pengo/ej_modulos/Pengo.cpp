23ffb04d4e3d28c4c444fafdea4de8ef97f8bbb3
Alex <33158086+AlejandroDCastro@users.noreply.github.com>
2020-04-15 14:15:26 +0200

    Update Pengo.cpp

--- Pengo/ej_modulos/Pengo.cpp
+++ Pengo/ej_modulos/Pengo.cpp
@@ -1,1 +1,18 @@
+#include "Pengo.h"
 
+
+void Pengo::Update(float deltaTime) {
+    
+    // If he is in movement he can not set his direction
+    if (!isWalking) {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+            
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+            
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+            
+        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+            
+        }
+    }
+}
